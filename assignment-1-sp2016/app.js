1cfdbc17b513f0552e4bb4ba38cdb329dd76ec03
quyenvu917 <quyenvu917@gmail.com>
2016-05-04 18:27:03 +0000

    Assignment 3 COMPLETE

--- assignment-1-sp2016/app.js
+++ assignment-1-sp2016/app.js
@@ -5,18 +5,16 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
+var dbConfig = require('./data/db.js');
 var mongoose = require('mongoose');
 require('./models/Business');
 mongoose.connect('mongodb://localhost/business_enterprise');
-
-var routes = require('./routes/index');
-var users = require('./routes/users');
 
 var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
+app.set('view engine', 'jade');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
@@ -26,8 +24,25 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+// Configuring Passport
+var passport = require('passport');
+var expressSession = require('express-session');
+// TODO - Why Do we need this key ?
+app.use(expressSession({secret: 'mySecretKey'}));
+app.use(passport.initialize());
+app.use(passport.session());
+
+ // Using the flash middleware provided by connect-flash to store messages in session
+ // and displaying in templates
+var flash = require('connect-flash');
+app.use(flash());
+
+// Initialize Passport
+var initPassport = require('./passport/init');
+initPassport(passport);
+
+var routes = require('./routes/index')(passport);
 app.use('/', routes);
-app.use('/users', users);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
---
+++ assignment-1-sp2016/fb.js
@@ -0,0 +1,6 @@
+// facebook app settings - fb.js
+module.exports = {
+  'appID' : '1184686371576396',
+  'appSecret' : '129b99169d8781d2e49aaa575f844714',
+  'callbackUrl' : 'https://assignment-1-sp2016-quyenvu917.c9users.io/login/facebook/callback'
+}
